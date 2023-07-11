bb8a774ec8833c371028e07ec59a7a4b4ea6f21e
Demyan <demuanivanytsky8@gmail.com>
2023-07-11 15:46:45 +0300

    first

---
+++ calc/js/script.js
@@ -0,0 +1,21 @@
+"use strict";
+
+const inputUah = document.querySelector("#uah"),
+      inputUsd = document.querySelector("#usd");
+
+inputUah.addEventListener("input", () => { 
+    const request = new XMLHttpRequest();
+
+    request.open("GET", "js/current1.json");
+    request.setRequestHeader("Content-type", "application/json; charset=utf-8");
+    request.send();
+
+    request.addEventListener("load", () => { 
+        if ( request.status === 200) {
+            const data = JSON.parse(request.response);
+            inputUsd.value = (+inputUah.value / data.current.usd).toFixed(2);
+        } else { 
+            inputUsd.value = "Something went wrong"
+        }
+    });
+});
